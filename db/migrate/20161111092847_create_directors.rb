47dbb6ff63672d7f1e16e5d7767e5b02ea204c87
Murugan <murugu2012@gmail.com>
2016-11-11 14:58:47 +0530

    Generate directors resource with starter:resource

---
+++ db/migrate/20161111092847_create_directors.rb
@@ -0,0 +1,17 @@
+class CreateDirectors < ActiveRecord::Migration
+  def change
+    create_table :directors do |t|
+      t.string :name
+      t.date :dob
+      t.integer :age
+      t.text :bio
+      t.boolean :oscar
+      t.string :image
+      t.string :address
+      t.time :show_time
+
+      t.timestamps
+
+    end
+  end
+end
